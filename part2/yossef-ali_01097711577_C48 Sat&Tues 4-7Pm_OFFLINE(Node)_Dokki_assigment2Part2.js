9572aa8e36077ccbce863b041b1e70b568c08da5
yossef <yossefali55555555@gmail.com>
2026-08-07 00:50:26 +0300

    LAST EDIT

--- part2/yossef-ali_01097711577_C48 Sat&Tues 4-7Pm_OFFLINE(Node)_Dokki_assigment2Part2.js
+++ part2/yossef-ali_01097711577_C48 Sat&Tues 4-7Pm_OFFLINE(Node)_Dokki_assigment2Part2.js
@@ -12,11 +12,21 @@
 //         req.on("end",()=>{
 //             data=JSON.parse(data)
 //             file=JSON.parse(file)
-//             file.push(data)
+//             const user= file.find((ele)=>{
+//                 return ele.email===data.email;
+//             })
+//             if(user){
+//                 res.writeHead(400)
+//                 res.write("bad request")
+//                 res.end()
+//             }
+//             else{
+//              file.push(data)
 //             fs.writeFileSync("./users.json",JSON.stringify(file))
 //             res.writeHead(201)
 //             res.write("user added")
 //             res.end()
+//             }
 //         })
 //     }
 //     else{
@@ -121,16 +131,17 @@ http.createServer((req,res)=>{
     let file = JSON.parse(fs.readFileSync("./users.json","utf-8"))
     let id = Number(url.split("/")[2])//["", "user", "5"] so number is index: 2
     if (url==`/user/${id}` && method=="GET"){
-        res.writeHead(200)
         let finding=file.find((ele)=>{
             return ele.id==id;
         })
-        res.write(JSON.stringify(finding))
         if (!finding) {
              res.writeHead(404);
              res.end("User not found");
 }
-        res.end()
+ else {
+    res.writeHead(200);
+    res.end(JSON.stringify(finding));
+}
     }
     else{
         res.writeHead(404)
